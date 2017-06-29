85b75cef591c09a4395320e2e618a17077f82f19
Walter4444 <walter.vega.95@hotmail.com>
2017-06-29 02:23:16 -0500

    Se creó la clase Matter

---
+++ src/pe/edu/utp/deco/models/Matter.java
@@ -0,0 +1,38 @@
+package pe.edu.utp.deco.models;
+
+/**
+ * Created by Usuario on 29/06/2017.
+ */
+public class Matter {
+    private String id;
+    private String name;
+
+    public Matter(String id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public Matter() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public Matter setId(String id) {
+        this.id = id;
+        return this;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Matter setName(String name) {
+        this.name = name;
+        return this;
+
+    }
+}
