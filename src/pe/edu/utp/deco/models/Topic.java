f8ccbbe729a58013589d577bd2fb4f6326745e9d
Gustavo <1411858@utp.edu.pe>
2017-06-29 02:55:21 -0500

    Agregando atributos y metodos a la clase Topic

--- src/pe/edu/utp/deco/models/Topic.java
+++ src/pe/edu/utp/deco/models/Topic.java
@@ -5,8 +5,43 @@ package pe.edu.utp.deco.models;
  */
 public class Topic{
 
-    String id;
-    Matter matter;
-    
+    private String id;
+    private Matter matter;
+    private String description;
 
+    public Topic() {
+    }
+
+    public Topic(String id, Matter matter, String description) {
+        this.id = id;
+        this.matter = matter;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Topic setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public Matter getMatter() {
+        return matter;
+    }
+
+    public Topic setMatter(Matter matter) {
+        this.matter = matter;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Topic setDescription(String description) {
+        this.description = description;
+        return this;
+    }
 }
